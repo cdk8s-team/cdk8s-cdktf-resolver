4523944c620e6ed5b50c18970b43facc29fbf7e0
epolon <epolon@amazon.com>
2023-09-12 12:00:54 +0300

    projen
    Signed-off-by: epolon <epolon@amazon.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,31 @@
-import { Cdk8sTeamJsiiProject } from "@cdk8s/projen-common";
+import { Cdk8sTeamJsiiProject } from '@cdk8s/projen-common';
 const project = new Cdk8sTeamJsiiProject({
-  defaultReleaseBranch: "main",
-  name: "cdk8s-cdktf-resolver",
+  defaultReleaseBranch: 'main',
+  name: 'cdk8s-cdktf-resolver',
   projenrcTs: true,
-  release: false,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  release: true,
+  devDeps: ['@cdk8s/projen-common', 'cdktf-cli', 'cdk8s-cli'],
+  peerDeps: ['cdktf', 'cdk8s', 'constructs'],
+  jsiiVersion: '^5',
 });
+
+// ignore integ tests because we will add a dedicated task
+// for them that only runs on release
+project.jest?.addIgnorePattern('/test/integ/');
+
+const integTask = project.addTask('integ');
+integTask.exec(jest('integ/integ.test.ts'));
+
+// run integ on release.
+// we don't run it on each PR because it brings security and operational
+// issues which are not worth the effort at this moment.
+const releaseTask = project.tasks.tryFind('release')!;
+releaseTask.exec(`npx projen ${integTask.name}`);
+
 project.synth();
+
+function jest(args: string) {
+  // we override 'testPathIgnorePatterns' and 'testMatch' so that it matches only integration tests
+  // see https://github.com/jestjs/jest/issues/7914
+  return `jest --testMatch "<rootDir>/test/integ/**/*.test.ts" --testPathIgnorePatterns "/node_modules/" --passWithNoTests --all --updateSnapshot --coverageProvider=v8 ${args}`;
+};
