92169d8284af6beb92c07d46b5d8ce45e0718b80
epolon <epolon@amazon.com>
2023-09-19 16:35:13 +0300

    mid work
    Signed-off-by: epolon <epolon@amazon.com>

--- src/fetch-output-value.ts
+++ src/fetch-output-value.ts
@@ -10,13 +10,12 @@ async function fetchOutputValue(projectDir: string, appDir: string, stackName: s
     execSync(`cdktf output --skip-synth --output ${appDir} ${stackName} --outputs-file ${outputsFile}`, {
       cwd: projectDir,
     });
+    const outputs = JSON.parse(fs.readFileSync(path.join(projectDir, outputsFile), { encoding: 'utf-8' }));
+    return outputs[stackName][outputName];
   } finally {
     // lets not leave any leftovers
     fs.unlinkSync(path.join(projectDir, outputsFile));
   }
-
-  const outputs = JSON.parse(fs.readFileSync(path.join(projectDir, outputsFile), { encoding: 'utf-8' }));
-  return outputs[stackName][outputName];
 
 }
 
@@ -24,6 +23,6 @@ fetchOutputValue(process.argv[2], process.argv[3], process.argv[4], process.argv
   .then(d => {
     console.log(JSON.stringify(d));
   })
-  .catch(_ => {
-    throw new Error(`${process.argv[2], process.argv[3], process.argv[4], process.argv[5]}`);
+  .catch(e => {
+    throw e;
   });
