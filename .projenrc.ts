d1c9fdd71ba110ff2327212fab3834a92a0a67eb
epolon <epolon@amazon.com>
2023-10-03 12:26:44 +0300

    install terraform
    Signed-off-by: epolon <epolon@amazon.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -17,6 +17,18 @@ const project = new Cdk8sTeamJsiiProject({
         'role-session-name': 'cdk8s-awscdk-resolver-release',
       },
     },
+    {
+      name: 'Install terraform | GPG',
+      run: 'wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg',
+    },
+    {
+      name: 'Install terraform | Sources',
+      run: 'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list',
+    },
+    {
+      name: 'Install terraform | Binary',
+      run: 'sudo apt update && sudo apt install terraform',
+    },
   ],
   npmAccess: NpmAccess.PUBLIC,
 });
